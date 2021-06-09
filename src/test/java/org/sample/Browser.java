9f4e7b11397884469803588b0606c3bcb16c5ddd
Sabasiva06 <sabasiva06@gmail.com>
2021-06-09 10:38:19 +0530

    Maven Sample Project

---
+++ src/test/java/org/sample/Browser.java
@@ -0,0 +1,14 @@
+package org.sample;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Browser {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "D:\\MavenSampleProgramm\\Driver\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.facebook.com/");
+	}
+}
